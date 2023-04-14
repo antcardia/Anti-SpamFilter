dff46b6702e090dd0da6cbb79d12972f83440c21
antcardia <antcardia4@alum.us.es>
2023-04-14 12:48:46 +0200

    Pequeños arreglos

--- src/main/java/antiSpamFilter/AntiSpamFilter.java
+++ src/main/java/antiSpamFilter/AntiSpamFilter.java
@@ -4,24 +4,32 @@ package antiSpamFilter;
 import java.util.Arrays;
 import java.util.List;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class AntiSpamFilter {
 
-	static Double threshold = 0.1;
+	protected Double	threshold;
+
+	protected String	spam;
 
 
-	public static boolean isSpam(final String text) {
+	public AntiSpamFilter(final Double threshold, final String spam) {
+		this.threshold = threshold;
+		this.spam = spam;
+	}
+
+	public boolean isSpam(final String text) {
 		final String words = text.toLowerCase().replaceAll("\\s+", " ");
-		final String[] spam = AntiSpamFilter.getSpamTerms().toLowerCase().replaceAll("\\s+", " ").replaceAll(", ", ",").split(",");
+		final String[] spam = this.spam.toLowerCase().replaceAll("\\s+", " ").replaceAll(", ", ",").split(",");
 		int count = 0;
 		final List<String> word = Arrays.asList(words.split(" "));
 		for (final String sp : spam)
 			count += words.split(sp).length - 1;
 		final double porcentajeSpam = count / (double) word.size();
-		return porcentajeSpam > AntiSpamFilter.threshold;
-	}
-
-	private static String getSpamTerms() {
-		return "sex, viagra, cialis, one million, you've won, nigeria, sexo, un millón, has ganado";
+		return porcentajeSpam > this.threshold;
 	}
 
 }
--- src/test/java/antiSpamFilterTest/AntiSpamFilterTest.java
+++ src/test/java/antiSpamFilterTest/AntiSpamFilterTest.java
@@ -5,31 +5,34 @@ import antiSpamFilter.AntiSpamFilter;
 
 public class AntiSpamFilterTest {
 
-	public static void isSpamTest() {
+	public void isSpamTest() {
+		final AntiSpamFilter spam = new AntiSpamFilter(0.1, "sex, viagra, cialis, one million, you've won, nigeria, sexo, un millón, has ganado");
+		spam.setSpam("sex, viagra, cialis, one million, you've won, nigeria, sexo, un millón, has ganado");
 		final String text1 = "Hola, ¿qué tal estás?";
 		final String text2 = "NIGERIA NIGERIA";
 		final String text3 = "Hola que tal, me gusta mucho el sexo nocturno";
 		final String text4 = "¡Vaya, YOU'VE WON!";
-		if (AntiSpamFilter.isSpam(text1))
+		if (spam.isSpam(text1))
 			System.out.println("El texto " + text1 + " es spam");
 		else
 			System.out.println("El texto " + text1 + " no es spam");
-		if (AntiSpamFilter.isSpam(text2))
+		if (spam.isSpam(text2))
 			System.out.println("El texto " + text2 + " es spam");
 		else
 			System.out.println("El texto " + text2 + " no es spam");
-		if (AntiSpamFilter.isSpam(text3))
+		if (spam.isSpam(text3))
 			System.out.println("El texto " + text3 + " es spam");
 		else
 			System.out.println("El texto " + text3 + " no es spam");
-		if (AntiSpamFilter.isSpam(text4))
+		if (spam.isSpam(text4))
 			System.out.println("El texto " + text4 + " es spam");
 		else
 			System.out.println("El texto " + text4 + " no es spam");
 	}
 
 	public static void main(final String[] args) {
-		AntiSpamFilterTest.isSpamTest();
+		final AntiSpamFilterTest antiSpamFilterTest = new AntiSpamFilterTest();
+		antiSpamFilterTest.isSpamTest();
 
 	}
 
